26be90d4ebbb7b37b56e4f3f4b9bd8ee961d91fc
fujiyamaorange <kaito.bakuman@gmail.com>
2023-03-04 17:44:36 +0900

    remove loading video for the first time

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
-import path from 'node:path';
-
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
-import { getFileList } from './tools/get_file_list';
-
-const publicDir = path.resolve(__dirname, './public');
-const getPublicFileList = async (targetPath: string) => {
-  const filePaths = await getFileList(targetPath);
-  const publicFiles = filePaths
-    .map((filePath) => path.relative(publicDir, filePath))
-    .map((filePath) => path.join('/', filePath));
-
-  return publicFiles;
-};
-
 export default defineConfig(async () => {
-  const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
+  // const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
 
   return {
     build: {
@@ -27,13 +14,24 @@ export default defineConfig(async () => {
       assetsInlineLimit: 4096,
       cssCodeSplit: true,
       minify: true,
+      reportCompressedSize: false,
       // デフォルトが0なので設定しない
       // rollupOptions: {
       //   output: {
       //     experimentalMinChunkSize: 40960,
       //   },
       // },
-      target: 'chrome87',
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom']
+          }
+        },
+        plugins: [
+          visualizer(),
+        ],
+      },
+      target: 'chrome87'
     },
     plugins: [
       react(),
@@ -42,7 +40,6 @@ export default defineConfig(async () => {
       ViteEjsPlugin({
         module: '/src/client/index.tsx',
         title: '買えるオーガニック',
-        videos,
       }),
     ],
   };
