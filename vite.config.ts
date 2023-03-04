d77232ab5aa2aa6f71cd75a9981245574fb14073
fujiyamaorange <kaito.bakuman@gmail.com>
2023-03-04 17:57:35 +0900

    remove unused vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig(async () => {
@@ -37,20 +36,6 @@ export default defineConfig(async () => {
       react(),
       // TODO: トップレベルawait使っているかどうか確認
       topLevelAwait(),
-      ViteEjsPlugin({
-        module: '/src/client/index.tsx',
-        title: '買えるオーガニック',
-      }),
     ],
   };
 });
-
-
-// dist/index.html                          0.64 kB
-// dist/assets/canvaskit-2a9e3015.wasm  6,836.23 kB
-// dist/assets/index-ec4c69a8.js        8,940.68 kB │ gzip: 3,814.77 kB
-
-// (!) Some chunks are larger than 500 kBs after minification. Consider:
-// - Using dynamic import() to code-split the application
-// - Use build.rollupOptions.output.manualChunks to improve chunking: https://rollupjs.org/configuration-options/#output-manualchunks
-// - Adjust chunk size limit for this warning via build.chunkSizeWarningLimit.
