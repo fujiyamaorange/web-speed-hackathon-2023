7ba87f9394f1eb3f32a981b5173a8785936436b4
fujiyamaorange <kaito.bakuman@gmail.com>
2023-03-04 16:48:29 +0900

    update icon to remove build size

--- src/client/components/foundation/Icon/Icon.tsx
+++ src/client/components/foundation/Icon/Icon.tsx
@@ -1,21 +1,59 @@
 import classNames from 'classnames';
-import type { FC } from 'react';
-import * as Icons from 'react-icons/fa';
+import { FaArrowLeft, FaArrowRight, FaCheckCircle, FaPlay, FaShoppingCart, FaUser } from 'react-icons/fa';
 
 import * as styles from './Icon.styles';
 
 type Props = {
-  type: keyof typeof Icons;
+  type: string;
   width: number;
   height: number;
   color: string;
 };
 
-export const Icon: FC<Props> = ({ color, height, type, width }) => {
-  const Icon = Icons[type];
+export const FaArrowLeftIcon = ({ color, height, type, width }: Props) => {
   return (
     <span className={classNames(type, styles.container({ color, height, width }))}>
-      <Icon />
+      <FaArrowLeft />
+    </span>
+  );
+};
+
+export const FaArrowRightIcon = ({ color, height, type, width }: Props) => {
+  return (
+    <span className={classNames(type, styles.container({ color, height, width }))}>
+      <FaArrowRight />
+    </span>
+  );
+};
+
+export const FaShoppingCartIcon = ({ color, height, type, width }: Props) => {
+  return (
+    <span className={classNames(type, styles.container({ color, height, width }))}>
+      <FaShoppingCart />
+    </span>
+  );
+};
+
+export const FaUserIcon = ({ color, height, type, width }: Props) => {
+  return (
+    <span className={classNames(type, styles.container({ color, height, width }))}>
+      <FaUser />
+    </span>
+  );
+};
+
+export const FaPlayIcon = ({ color, height, type, width }: Props) => {
+  return (
+    <span className={classNames(type, styles.container({ color, height, width }))}>
+      <FaPlay />
+    </span>
+  );
+};
+
+export const FaCheckCircleIcon = ({ color, height, type, width }: Props) => {
+  return (
+    <span className={classNames(type, styles.container({ color, height, width }))}>
+      <FaCheckCircle />
     </span>
   );
 };
